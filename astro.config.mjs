0394c75453b96d92c1ca7c13ddc46cc84afa09ef
Aditya Pakki <aditya.pakki@outlook.com>
2026-02-21 12:42:28 -0500

    add robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import preact from "@astrojs/preact";
 
 import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [preact(), icon()],
+  integrations: [preact(), icon(), sitemap()],
+  site: "https://www.twilightpsychology.com",
 });
