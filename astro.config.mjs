db286c38ec42cffddc146a252500712880005fd6
Aditya Pakki <aditya.pakki@outlook.com>
2026-02-20 19:43:18 -0500

    add footer info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from '@tailwindcss/vite';
 import preact from "@astrojs/preact";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [preact()],
+  integrations: [preact(), icon()],
 });
