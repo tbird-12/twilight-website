3814a56478f3cd4eaa2ad9bd22d16ae917668340
aditya pakki <aditya.pakki@outlook.com>
2026-03-22 19:43:59 -0400

    update the domain for sign in and widget

--- src/data/resource.ts
+++ src/data/resource.ts
@@ -7,7 +7,7 @@ export const FAX_NUMBER_FORMATTED = "(502) 324-3210";
 
 export const EMAIL_ADDRESS = "office@twilightpsychology.com";
 
-export const SIGN_IN_LINK = "https://heatherlcornett.clientsecure.me/sign-in";
+export const SIGN_IN_LINK = "https://twilightpsychology.clientsecure.me/sign-in";
 
-export const WIDGET_LINK = "https://heatherlcornett.clientsecure.me/contact-widget";
+export const WIDGET_LINK = "https://twilightpsychology.clientsecure.me/contact-widget";
 export const REFERRAL_LINK = "https://drive.google.com/file/d/1fhYlXyIpFgUslpYGLkkt6xYveGfmlSG2/view";
